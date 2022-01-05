1a271e3b34d383f165dc344e08b7526ea4c24d74
Xingchen <Xingchen1224@users.noreply.github.com>
2022-01-05 20:32:50 +0800

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,7 +1,8 @@
+sudo apt update -y
 sudo apt install build-essential cmake pkg-config unzip yasm git checkinstall -y
 sudo apt install libjpeg-dev libpng-dev libtiff-dev -y
-sudo apt-get install gcc-10 g++11 -y
+sudo apt-get install gcc-10 g++10 -y
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 10
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 10
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-10 10
 sudo update-alternatives --config gcc
 sudo update-alternatives --config g++
