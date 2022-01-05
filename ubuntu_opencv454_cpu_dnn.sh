aadcd8848bead23c612aedfa3e83c25e1213c712
Xingchen <Xingchen1224@users.noreply.github.com>
2022-01-05 20:29:01 +0800

    Update ubuntu_opencv454_cpu_dnn.sh

--- ubuntu_opencv454_cpu_dnn.sh
+++ ubuntu_opencv454_cpu_dnn.sh
@@ -1,11 +1,3 @@
-sudo apt install build-essential cmake pkg-config unzip yasm git checkinstall -y
-sudo apt install libjpeg-dev libpng-dev libtiff-dev -y
-sudo apt-get install gcc-10 g++11 -y
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 10
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 10
-sudo update-alternatives --config gcc
-sudo update-alternatives --config g++
-
 OPENCV_VERSION=4.5.4
 wget -O opencv.zip https://github.com/opencv/opencv/archive/refs/tags/$OPENCV_VERSION.zip
 unzip -o opencv.zip
