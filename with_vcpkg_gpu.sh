f1f6924a72167a40768953037090e80af1fc66c0
Xingchen <Xingchen1224@users.noreply.github.com>
2022-09-15 16:51:21 +0800

    Update with_vcpkg_gpu.sh

--- with_vcpkg_gpu.sh
+++ with_vcpkg_gpu.sh
@@ -2,6 +2,8 @@ set -e
 
 sudo apt install autoconf bison gperf libtool libx11-dev libxkbcommon-x11-dev libx11-xcb-dev libxft-dev libxext-dev libgles2-mesa-dev libxi-dev libgl1-mesa-dev libglu1-mesa-dev mesa-common-dev libxrandr-dev libxxf86vm-dev -y
 
+sudo apt install build-essential tar curl zip unzip libxcursor-dev libgtk2.0-dev -y
+
 git clone https://github.com/microsoft/vcpkg.git
 
 cd vcpkg && sh bootstrap-vcpkg.sh
