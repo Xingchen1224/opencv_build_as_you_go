dcb17ac6505867ad801939c8f4ccc2fe9981fedb
Xingchen <Xingchen1224@users.noreply.github.com>
2022-09-15 15:32:45 +0800

    Update with_vcpkg.sh

--- with_vcpkg.sh
+++ with_vcpkg.sh
@@ -1,25 +1,25 @@
 set -e
 
-apt update -y
+sudo apt update -y
 
-apt install git -y
+sudo apt install git -y
 
-apt install curl zip unzip tar -y
+sudo apt install curl zip unzip tar -y
 
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-apt install build-essential tar curl zip unzip libxcursor-dev libgtk2.0-dev -y
+sudo apt install build-essential tar curl zip unzip libxcursor-dev libgtk2.0-dev -y
 
 # Common Tool for opencv4
-apt install libx11-dev libxkbcommon-x11-dev libx11-xcb-dev libxft-dev libxext-dev -y
+sudo apt install libx11-dev libxkbcommon-x11-dev libx11-xcb-dev libxft-dev libxext-dev -y
 
-apt install libxi-dev libgl1-mesa-dev libglu1-mesa-dev mesa-common-dev libxrandr-dev libxxf86vm-dev -y
+sudo apt install libxi-dev libgl1-mesa-dev libglu1-mesa-dev mesa-common-dev libxrandr-dev libxxf86vm-dev -y
 
-apt install autoconf gperf libtool -y
+sudo apt install autoconf gperf libtool -y
 
-apt install bison -y
+sudo apt install bison -y
 
-apt install aria2 -y
+sudo apt install aria2 -y
 
 git clone https://github.com/microsoft/vcpkg.git
 
