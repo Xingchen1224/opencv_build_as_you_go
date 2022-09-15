a9147ddf790f40ae6940c1dca12d12272864eee1
Xingchen <Xingchen1224@users.noreply.github.com>
2022-09-15 16:15:40 +0800

    Update with_vcpkg_gpu.sh

--- with_vcpkg_gpu.sh
+++ with_vcpkg_gpu.sh
@@ -1,6 +1,6 @@
 set -e
 
-sudo apt install libgles2-mesa-dev -y
+sudo apt install autoconf bison gperf libtool libx11-dev libxkbcommon-x11-dev libx11-xcb-dev libxft-dev libxext-dev libgles2-mesa-dev libxi-dev libgl1-mesa-dev libglu1-mesa-dev mesa-common-dev libxrandr-dev libxxf86vm-dev -y
 
 git clone https://github.com/microsoft/vcpkg.git
 
